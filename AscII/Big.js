5ab0215d0c1e86d92030b50a7b0ae7cdadedd50a
MovementGH <48330137+MovementGH@users.noreply.github.com>
2021-02-16 17:25:35 -0500

    Add AscII Challenge

---
+++ AscII/Big.js
@@ -0,0 +1,13 @@
+Size=process.argv[2];
+Rotation=process.argv[3];
+Input=[...process.argv[4]]
+Art=[]
+Map='─│ ━┃ ┄┆ ┅┇ ┈┊ ┉┋ ┌┐┘└ ┍┒┙┖ ┎┑┚┕ ┏┓┛┗ ├┬┤┴ ┝┰┥┸ ┞┮┧┵ ┟┭┦┶ ┠┯┨┷ ┡┲┪┹ ┢┱┩┺ ┣┳┫┻ ┽╀┾╁ ┿╂ ╃╄╆╅ ╇╊╈╉ ╌╎ ╍╏ ═║ ╒╖╛╙ ╓╕╜╘ ╔╗╝╚ ╞╥╡╨ ╟╤╢╧ ╠╦╣╩ ╪╫ ╬ ╭╮╯╰ ╱╲ ╳ ╴╵╶╷ ╸╹╺╻ ╼╽╾╿';
+RotateChar=(Char)=>{
+    var Index=Map.indexOf(Char);
+    return Index==-1?Char:Map[Map[Index+1]==' '?Map.match('\\S+'+Char).index:Index+1];
+}
+while(Input.length)Art.push(Input.splice(0,Size))
+while(Rotation%360>0)Art=Art.map((row,i)=>row.map((val, j)=>RotateChar(Art[Art.length-1-j][i]))),Rotation-=90
+
+console.log(Art.map(a=>a.join``).join``)
